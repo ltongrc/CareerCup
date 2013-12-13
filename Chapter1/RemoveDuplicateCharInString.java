b13f015b7a4a5342d104fa7ca4ef40a9de742e6c
ltongrc <tongluruc@gmail.com>
2013-12-13 16:03:26 -0500

    Remove duplicate characters in a string

--- Chapter1/RemoveDuplicateCharInString.java
+++ Chapter1/RemoveDuplicateCharInString.java
@@ -1,4 +1,27 @@
+import java.util.HashMap;
 public class RemoveDuplicateCharInString{
+    public static String remove(String s){
+       if(s == null || s.length() == 1)
+            return s;
+        int[] charSet = new int[256];
+        for(int i=0; i < s.length(); i++){
+            int index = (int)s.charAt(i);
+            charSet[index]++;
+        }
 
+        String result = "";
+        for(int i = 0; i < charSet.length; i++){
+            if(charSet[i]!=0){
+                result += (char)i;
+            }
+        }
+        return result;
+    }   
 
+    public static void main(String args[]){
+        String[] test = {"aaaa", "ababa", "abcd"};
+        for(String s : test){
+            System.out.println( s + "  " + remove(s));
+        }
+    }
 }
