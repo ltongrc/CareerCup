d316cd058e97fe6d7561b1d9946024bcfc50a297
ltongrc <tongluruc@gmail.com>
2013-12-13 16:50:20 -0500

    Add a new function to RemoveDuplicate

--- Chapter1/RemoveDuplicateCharInString.java
+++ Chapter1/RemoveDuplicateCharInString.java
@@ -18,10 +18,38 @@ public class RemoveDuplicateCharInString{
         return result;
     }   
 
+    public static char[] removeAns(char[] str){
+        if(str == null || str.length == 1)
+            return str;
+        boolean[] hit = new boolean[256];
+        hit[str[0]] = true;
+        int tail = 1;
+        for(int i = 1; i < str.length; i++){
+            if(!hit[str[i]]){
+                str[tail] = str[i];
+                hit[str[tail]] = true;
+                tail++;
+            }
+        }
+        if(tail == str.length)
+            return str;
+        else{
+            char res[] = new char[tail]; 
+            for( int i = 0 ; i < tail; i++){
+                res[i] = str[i];
+            }
+            return res;
+        }
+    }
     public static void main(String args[]){
         String[] test = {"aaaa", "ababa", "abcd"};
         for(String s : test){
             System.out.println( s + "  " + remove(s));
         }
+        for(String s : test){
+            char[] temp = s.toCharArray();
+            System.out.println( s + " : " + new String(removeAns(temp)));
+        }
+
     }
 }
